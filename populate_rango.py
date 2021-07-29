e861473fbe359976d1de144dc039394a56ac653e
“Firstname <2528485w@student.gla.ac.uk>
2021-07-29 05:19:18 +0100

    chapter 6 exercise

--- populate_rango.py
+++ populate_rango.py
@@ -5,29 +5,28 @@ django.setup()
 from rango.models import Category,Page
 
 def populate():
-# First, we will create lists of dictionaries containing the pages
-# we want to add into each category.
-# Then we will create a dictionary of dictionaries for our categories. 
-# This might seem a little bit confusing, but it allows us to iterate 
-# through each data structure, and add the data to our models.
+
 
     python_pages = [
     {'title': 'Official Python Tutorial',
-    'url':'http://docs.python.org/3/tutorial/'}, 
+    'url':'http://docs.python.org/3/tutorial/',
+    'views':32}, 
     {'title':'How to Think like a Computer Scientist',
-    'url':'http://www.greenteapress.com/thinkpython/'}, 
+    'url':'http://www.greenteapress.com/thinkpython/',
+    'views':44}, 
     {'title':'Learn Python in 10 Minutes',
-    'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    'url':'http://www.korokithakis.net/tutorials/python/',
+    'views':55} ]
 
     django_pages = [
-        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-    {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'},
-    {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/'} ]
+        {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/','views':33},
+    {'title':'Django Rocks', 'url':'http://www.djangorocks.com/','views':43},
+    {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/','views':21} ]
 
     other_pages = [ {'title':'Bottle',
-    'url':'http://bottlepy.org/docs/dev/'}, 
+    'url':'http://bottlepy.org/docs/dev/','views':32}, 
     {'title':'Flask',
-    'url':'http://flask.pocoo.org'} ]
+    'url':'http://flask.pocoo.org','views':16} ]
 
     cats = {'Python': { 'pages': python_pages,'views':128, 'likes':64},
     'Django':{'pages': django_pages,'views':64 ,'likes':32},
@@ -36,7 +35,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat,views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- rango/views.py
+++ rango/views.py
@@ -5,14 +5,18 @@ from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages']=page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 
+def about(request):
+    return render(request, 'rango/about.html')
+
 def show_category(request, category_name_slug):
-# Create a context dictionary which we can pass # to the template rendering engine. context_dict = {}
     context_dict = {}
     try:
         category = Category.objects.get(slug=category_name_slug)
